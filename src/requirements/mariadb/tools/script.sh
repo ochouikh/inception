d8b9de2a77de713b0c58efb59dfbcbe4b961c13a
ochouikh <othman-chouikhane@outlook.fr>
2024-03-03 16:37:18 +0100

    v0.1

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-service mysql start;
+sed -i 's/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf;
+
+service mariadb start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;";
 mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PSSWD}';";
 mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PSSWD}';";
--- src/requirements/wordpress/tools/script.sh
+++ src/requirements/wordpress/tools/script.sh
@@ -8,6 +8,10 @@ wp config set DB_HOST 'mariadb:3306' --allow-root --path=/var/www/html;
 wp core install --url=$DOMAIN_NAME --title=INCEPTION --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root --path=/var/www/html;
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root --path=/var/www/html;
 
+cp -r /var/www/html/* /var/www/wordpress
 
 sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#' /etc/php/7.4/fpm/pool.d/www.conf;
+
+mkdir -p /run/php/
+
 /usr/sbin/php-fpm7.4 -F;
